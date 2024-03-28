2a93826e665965b33e271dbd26e54592a186df1f
Yahya Murad <59206482+YahyaMurad@users.noreply.github.com>
2024-03-28 22:29:40 +0300

    Commenting and Formatting

--- interactor.py
+++ interactor.py
@@ -1,22 +1,24 @@
-from selenium import webdriver
+import platform
 from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
-import platform
 
-driver = webdriver.Chrome()
+# Initialize driver
+driver = webdriver.Chrome() # Add driver path
 
 driver.get("https://web.whatsapp.com")
 sleep(20)
 
+# Change NAME OF RECIPIENT
 user = driver.find_element(By.XPATH, "//*[contains(text(), '" + "NAME OF RECIPIENT" + "')]")
 
 user.click()
 
-word = "HELLO"
-x = 100
+word = "HELLO" # Word that is being sent
+x = 100 # Number of times it is being set
 
 for i in range(x):
     actions = ActionChains(driver)
