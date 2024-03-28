0e2741ae9717a4018aa6e433c280ab17a2c66eac
Yahya Murad <59206482+YahyaMurad@users.noreply.github.com>
2024-03-28 22:32:35 +0300

    Commenting and Formatting

--- whatsapp_sender.py
+++ whatsapp_sender.py
@@ -1,16 +1,20 @@
 import argparse
-from selenium import webdriver
 from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+
 
 def send_messages(contact_name, message, num_times):
     driver = webdriver.Chrome()
     driver.get("https://web.whatsapp.com")
     sleep(20)
 
-    user = driver.find_element(By.XPATH, "//*[contains(text(), '" + contact_name + "')]")
+    user = driver.find_element(
+        By.XPATH, "//*[contains(text(), '" + contact_name + "')]"
+    )
     user.click()
 
     for _ in range(num_times):
@@ -21,11 +25,16 @@ def send_messages(contact_name, message, num_times):
     while True:
         pass
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Send messages on WhatsApp using Selenium.")
+    parser = argparse.ArgumentParser(
+        description="Send messages on WhatsApp using Selenium."
+    )
     parser.add_argument("--name", required=True, help="Name of the contact")
     parser.add_argument("--word", required=True, help="Word to send")
-    parser.add_argument("--times", type=int, required=True, help="Number of times to send the word")
+    parser.add_argument(
+        "--times", type=int, required=True, help="Number of times to send the word"
+    )
 
     args = parser.parse_args()
 
